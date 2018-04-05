165b6e55abaa842f9b1d53027fb0e06212cb185e
Christopher Helms <chris.helms.C@gmail.com>
2018-04-05 01:42:05 -0400

    Modifications to config and role, added simple dynamo insert.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 'use strict';
 const Alexa = require('alexa-sdk');
+const AWS = require('aws-sdk');
 
 const APP_ID = process.env.APP_ID;
+
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 const handlers = {
   'LaunchRequest': function() {
@@ -9,6 +12,32 @@ const handlers = {
   },
   'AddDebtIntent': function() {
     this.emit(':tell', 'Adding debt');
+
+    //grab iou variables from event, create item for databse
+    let deviceId = this.event.context.System.device.deviceId;
+    let slots = this.event.request.intent.slots;
+    var params = {
+      TableName: process.env.IOU_TABLE,
+      Item: {
+        device_id: deviceId,
+        borrower: slots.Borrower.value,
+        iou: {
+          creditor: slots.Creditor.value,
+          amount: slots.Amount.value,
+          category: slots.Category.value,
+          created: new Date(Date.now()).toLocaleString()
+        }
+      }
+    }
+
+    //insert info into database
+    docClient.put(params, function(err, data) {
+      if(err) {
+        console.error("Unable to add IOU. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("Creation of new IOU row succeeded.");
+      }
+    })
   },
   'AddRoommateIntent': function() {
     this.emit(':tell', 'Adding roommate');
